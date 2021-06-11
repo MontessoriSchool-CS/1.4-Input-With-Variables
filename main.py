87ba9190e80495a3ceb0b36e63965b435ebd7c9f
CompSciMrF <17910442+geekyteacher@users.noreply.github.com>
2021-06-11 10:00:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+# FORK ME or copy the code!  Please don't request edit access. This is the original so it needs to stay undedited for all users.
+
+# Input With Variables
+
+#Task 1 - Add comments to the code below to predict what will happen when it is run.
+
+#Task 2 - add comments to the code to explain what the variables are called and where they are used. Make sure to show where the variable.
+
+print("Hi! What's your name?")
+
+name = "Dave"   #name is a variable to hold someones first name
+
+print("Hi " + name + "! How are you today?") #Name ius used within the string
+
+
+
+# Task 3 - Adapt the code so that it assigns input into the 'name' variable. CHALLENGE - put a prompt in the input command to ask the user for their name. 
+
+# Task 4 - Combine lines 'Do you like programming' and answer = input  so that it the input has a prompt in it.
+
+#Task 5 - Adapt the output on the next to last line so that it includes both the name and the answer variables.
+
+name = "Billy"
+print("We want to know if you like progamming!")
+print()
+print("Do you like programming " + name + "?")
+answer = input()
+print("Great! You said " + answer + "!")
+print("Let's learn some Python today")
+
